98dc65869e3520b9a3965e5b4d006ea47ba940c3
bullzeye777 <jesus.aranda7@yahoo.com>
2021-08-24 21:00:26 -0500

    first commit

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,6 +1,19 @@
+/**
+ * Check if the number is even or odd
+ * isEven
+ * @param {number} num - the number that we are checking
+ * @returns true if the number is even
+ */
+
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if(num % 2 == 0){
+  console.log(`The ${num} is even`)
+  return true
+  } 
+  else {
+  return false
+}
 }
 
 const sum = (arr) => {
